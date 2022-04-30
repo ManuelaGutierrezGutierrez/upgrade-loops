aa2afe3daf54027c2aecefca63ff94e763cbd1a3
ManuelaGutierrezGutierrez <manuelagutierrezgutierrez@gmail.com>
2022-04-30 18:44:06 +0200

    add iteracion3

---
+++ iteracion3.js
@@ -0,0 +1,18 @@
+// **Iteración #3: Probando For...of**
+
+// Usa un bucle forof para recorrer todos los destinos del array. Imprime en un ***console.log*** sus valores.
+
+// Puedes usar este array:
+
+const placesToTravel = [
+	"Japon",
+	"Venecia",
+	"Murcia",
+	"Santander",
+	"Filipinas",
+	"Madagascar",
+];
+
+for (let place of placesToTravel) {
+	console.log(place);
+}
